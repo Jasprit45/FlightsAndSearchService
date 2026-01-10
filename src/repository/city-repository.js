95ee76a25b5515c533e2f6207b910c9e56b95a72
Jasprit45 <singhsardarjasprit@gmail.com>
2026-01-10 10:07:11 +0530

    Added filter

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,7 @@
+const {Op} = require('sequelize');
+
 const {City} = require('../models/index');
+
 
 //modal name singular
 //table name plural
@@ -51,8 +54,18 @@ class CityRepository {
         }
     }
 
-    async getAllCities(){
+    async getAllCities(filter){
         try {
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where:{
+                        name:{
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
             const cities = await City.findAll();
             return cities;
         } catch (error) {
