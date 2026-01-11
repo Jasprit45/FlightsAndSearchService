9eda92284a72805658e0d3a3a5c45567fcff9249
Jasprit45 <singhsardarjasprit@gmail.com>
2026-01-11 22:42:09 +0530

    Added few filters in flight(by min,max price)

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,7 +1,56 @@
 const {Flights} = require('../models/index');
-
+const {Op} = require('sequelize');
 
 class FlightRepository{
+
+    #createFilter(data) {
+        // data = {
+        //     data.flightNumber
+        //     data.airplaneId
+        //     data.arrivalAirportId
+        //     data.departureAirportId
+        //     data.arrivalTime
+        //     data.departureTime
+        //     data.price
+        //     data.boardingGate
+        //     data.totalSeats
+        // }
+        let filter = {}
+        if(data.arrivalAirportId) {
+            filter.arrivalAirportId = data.arrivalAirportId;
+        }
+        if(data.departureAirportId) {
+            filter.departureAirportId = data.departureAirportId;
+        }
+        
+        // if(data.minPrice){
+        //     Object.assign(filter,{price: {[Op.gte]: data.minPrice}});
+        // }
+        // if(data.maxPrice){
+        //     Object.assign(filter,{price: {[Op.lte]: data.maxPrice}});
+        // }
+        // if(data.minPrice && data.maxPrice ){
+        //     Object.assign(filter, {
+        //         [Op.and]:[
+        //         {price: {[Op.gte]: data.minPrice}},
+        //         {price: {[Op.lte]: data.maxPrice}}
+        //         ]
+        //     } )
+        // }
+
+        let priceFilter = [];
+        if(data.minPrice){
+            priceFilter.push({price: {[Op.gte]: data.minPrice}});
+        }
+        if(data.maxPrice){
+            priceFilter.push({price: {[Op.lte]: data.maxPrice}});
+        }
+        Object.assign(filter,{[Op.and]: priceFilter});
+        console.log(filter);
+        return filter;
+    }
+
+
     async createFlight(data) {
         try {
             const flight = await Flights.create(data);
@@ -11,6 +60,36 @@ class FlightRepository{
             throw{error};
         }
     }
+    async getFlight(flightid) {
+        try {
+            const flight = await Flights.findByPk(flightid);
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong in the flight-repository layer");
+            throw{error};
+        }
+    }
+    async getAllFlights(filter) {
+        try {
+            const filterObject = this.#createFilter(filter);
+            const flight = await Flights.findAll({
+                where: filterObject
+            });
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong in the flight-repository layer");
+            throw{error};
+        }
+    }
+
 }
 
 module.exports = FlightRepository;
+
+/*
+{
+    where: {
+        arrivalAirportId:2
+    }
+}
+ */
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,5 +1,5 @@
 const {FlightRepository , AirplaneRepository} = require('../repository/index');
-const {compareTime} = require('../utils/helper');
+const {compareDateTime} = require('../utils/helper');
 
 class FlightService {
     constructor(){
@@ -9,7 +9,7 @@ class FlightService {
 
     async createFlight(data) {
         try {
-            if(!compareTime(data.arrivalTime , data.departureTime)) {
+            if(!compareDateTime(data.arrivalTime , data.departureTime)) {
                 throw{error: 'Arrival time cannot be less than departure time.'};
             }
             const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
@@ -22,6 +22,17 @@ class FlightService {
             throw(error);
         }
     }
+
+    async getAllFlightData(data) {
+        try {
+            const flights = await this.flightRepository.getAllFlights(data);
+            return flights;
+        } catch (error) {
+            console.log("Something went wrong in the flight-service layer");
+            throw(error);
+        }
+    }
+
 }
 
 module.exports = FlightService;
@@ -31,7 +42,7 @@ module.exports = FlightService;
  * {
  * flightNumber
  * airplaneId
- * departureAirportId
+ * arrivalAirportId
  * departureAirportId
  * arrivalTime
  * departureTime
