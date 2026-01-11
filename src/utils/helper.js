040aaec7241d7c0273b374745628d02178d1db14
Jasprit45 <singhsardarjasprit@gmail.com>
2026-01-11 15:54:23 +0530

    Added datetime check in flightService(bussiness logic)

---
+++ src/utils/helper.js
@@ -0,0 +1,9 @@
+function compareTime(datetimeString1, datetimeString2) {
+    let dateTime1 = new Date(datetimeString1);
+    let dateTime2 = new Date(datetimeString2);
+    return ( dateTime1.getTime() > dateTime2.getTime() ) & (dateTime1.getDate() >= dateTime2.getDate() );
+}
+
+module.exports = {
+    compareTime,
+}
